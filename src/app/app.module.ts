d24e5287db88d742c6b9c5f3f89363192a29d0ba
Imantst1 <imantst1@gmail.com>
2019-06-10 19:05:51 +0000

    create entry and list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { EntryComponent } from './components/todo-list/entry/entry.component';
+import { ListComponent } from './components/todo-list/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    EntryComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
